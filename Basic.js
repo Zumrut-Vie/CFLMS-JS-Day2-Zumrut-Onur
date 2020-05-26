48844d7387a2d49ebe7718a31f6739734915afea
Zumrut-Vie <ekinci.z@gmail.com>
2020-05-26 12:55:50 +0200

    intermediate Ex1

--- Basic.js
+++ Basic.js
@@ -12,13 +12,5 @@ numberArray = [];
 var a = Math.max(...numberArray);
 console.log(a);
 
-//Basic Ex 3
-var temp = Math.floor(Math.random() * 32) + (-5) ;
-if(temp<10){
-	document.getElementById("img").src="./winter.jpg";
-}else if (temp > 10 && temp < 32 ){
-	document.getElementById("img").src="./rainy.jpg";
-	}else{
-    document.getElementById("img").src="./summer.jpg";
-	}
+//Basic Ex 3 on HTML
 
