d02a227802fa0332130581b4d8765124834c743b
Zumrut-Vie <ekinci.z@gmail.com>
2020-05-26 12:07:11 +0200

    script is in html file for the Ex3(Basic)

--- Basic.js
+++ Basic.js
@@ -1,16 +1,16 @@
 
 // Basic Ex 1
-/*var temp = Math.floor(Math.random() * 25) + (-5) ;
+var temp = Math.floor(Math.random() * 25) + (-5) ;
 if(temp<10){
 	console.log("The weather is cold");
 }else{
 	console.log("The weather is moderate");
-}*/
+}
 
 // Basic Ex 2 (with spread operator)
-/*numberArray = [];
+numberArray = [];
 var a = Math.max(...numberArray);
-console.log(a);*/
+console.log(a);
 
 //Basic Ex 3
 var temp = Math.floor(Math.random() * 32) + (-5) ;
